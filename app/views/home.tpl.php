2aa67396fdbbbf9f8e543fb02a29dbe85f434187
MarieLouPLevasseur <sunnymary007@hotmail.com>
2022-05-30 12:26:32 +0200

    HTML/CSS de la home

--- app/views/home.tpl.php
+++ app/views/home.tpl.php
@@ -1,1 +1,49 @@
-<h1>Je suis la page d'accueil</h1>
+<section>
+    <div class="content">
+        <!-- une carte pokemon -->
+        <div class="image-card">
+
+            <div class="image-item">
+                <img src="<?= $transversalVars['baseURL'] . "/img/1.png" ?>" alt="Pokemon" class="img-fluid">
+            </div>
+            <div class="image-title"><a href="#" class="image-link"> #PERSO et NOM PERSONNAGE </a>
+            </div>
+        </div>
+        <!-- fin de carte -->
+
+        <!-- une carte pokemon -->
+        <div class="image-card">
+
+            <div class="image-item">
+                <img src="<?= $transversalVars['baseURL'] . "/img/1.png" ?>" alt="Pokemon" class="img-fluid">
+            </div>
+            <div class="image-title"><a href="#" class="image-link">#PERSO et NOM PERSONNAGE </a>
+            </div>
+        </div>
+        <!-- fin de carte -->
+
+        <!-- une carte pokemon -->
+        <div class="image-card">
+
+            <div class="image-item">
+                <img src="<?= $transversalVars['baseURL'] . "/img/1.png" ?>" alt="Pokemon" class="img-fluid">
+            </div>
+            <div class="image-title"><a href="#" class="image-link">#PERSO et NOM PERSONNAGE </a>
+            </div>
+        </div>
+        <!-- fin de carte -->
+           <!-- une carte pokemon -->
+        <div class="image-card">
+
+            <div class="image-item">
+                <img src="<?= $transversalVars['baseURL'] . "/img/1.png" ?>" alt="Pokemon" class="img-fluid">
+            </div>
+            <div class="image-title"><a href="#" class="image-link">#PERSO et NOM PERSONNAGE </a>
+            </div>
+        </div>
+        <!-- fin de carte -->
+
+
+
+    </div>
+</section>
