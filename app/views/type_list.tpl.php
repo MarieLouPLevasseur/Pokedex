095b1b18e226f02c761a33cda03c94b5d126261f
MarieLouPLevasseur <sunnymary007@hotmail.com>
2022-05-31 09:41:46 +0200

    HTML page des Types

--- app/views/type_list.tpl.php
+++ app/views/type_list.tpl.php
@@ -1,1 +1,13 @@
-<h1>Je suis la page des Types</h1>
+<section class="types">
+<p>Cliquez sur un type pour voir tous les Pokémons de ce type</p>
+
+<div class="types-container">
+
+    <div class="typeItem" style="background-color:#COULEUR DU TYPE"><p><a href="#">NOM DU TYPE</a></p></div>
+    <div class="typeItem" style="background-color:#COULEUR DU TYPE"><p><a href="#">NOM DU TYPE</a></p></div>
+    <div class="typeItem" style="background-color:#COULEUR DU TYPE"><p><a href="#">NOM DU TYPE</a></p></div>
+    <div class="typeItem" style="background-color:#COULEUR DU TYPE"><p><a href="#">NOM DU TYPE</a></p></div>
+    <div class="typeItem" style="background-color:#COULEUR DU TYPE"><p><a href="#">NOM DU TYPE</a></p></div>
+
+</div>
+</section>
